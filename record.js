53f580c57bd32669f605f5d9e5238252c618c80a
hsuchingchun <48717305+hsuchingchun@users.noreply.github.com>
2023-12-01 22:22:36 +0800

    Update record.js

--- record.js
+++ record.js
@@ -3,29 +3,29 @@ let mediaRecorder;
 let lastRecordedBlob;
 let playbackRate = 3.5;
 
-    // 獲取麥克風許可權並開始錄音
-    document.getElementById('startRecord').addEventListener('click', async () => {
-      const startButton = document.getElementById('startRecord');
-      startButton.disabled = true;
+// 獲取麥克風許可權並開始錄音
+document.getElementById('startRecord').addEventListener('click', async () => {
+    const startButton = document.getElementById('startRecord');
+    startButton.disabled = true;
 
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaRecorder = new MediaRecorder(stream);
-      audioChunks = [];
-      document.getElementById('recordingIndicator').style.visibility = 'visible';
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder = new MediaRecorder(stream);
+    audioChunks = [];
+    document.getElementById('recordingIndicator').style.visibility = 'visible';
 
-      mediaRecorder.ondataavailable = e => {
+    mediaRecorder.ondataavailable = e => {
         audioChunks.push(e.data);
-      };
+    };
 
-      mediaRecorder.start();
-      document.getElementById('stopRecord').disabled = false;
-      document.getElementById('playbackSpeed').disabled = false;
-    });
+    mediaRecorder.start();
+    document.getElementById('stopRecord').disabled = false;
+    document.getElementById('playbackSpeed').disabled = false;
+});
 
-    // 停止錄音
-    document.getElementById('stopRecord').addEventListener('click', () => {
-      mediaRecorder.stop();
-      mediaRecorder.onstop = () => {
+// 停止錄音
+document.getElementById('stopRecord').addEventListener('click', () => {
+    mediaRecorder.stop();
+    mediaRecorder.onstop = () => {
         lastRecordedBlob = new Blob(audioChunks, { 'type': 'audio/wav' });
         const audio = new Audio(URL.createObjectURL(lastRecordedBlob));
         audio.playbackRate = playbackRate; // 設置播放速度為三倍
@@ -35,22 +35,22 @@ let playbackRate = 3.5;
         document.getElementById('startRecord').disabled = false;
         document.getElementById('playbackSpeed').disabled = false;
     };
-      document.getElementById('stopRecord').disabled = true;
-    });
+    document.getElementById('stopRecord').disabled = true;
+});
 
-    // 重播錄音
-    document.getElementById('replayRecord').addEventListener('click', () => {
-      if (lastRecordedBlob) {
+// 重播錄音
+document.getElementById('replayRecord').addEventListener('click', () => {
+    if (lastRecordedBlob) {
         const audio = new Audio(URL.createObjectURL(lastRecordedBlob));
         audio.playbackRate =  playbackRate;
         audio.play();
-      }
-    });
+    }
+});
 
-     // 更新播放速度及顯示數字值
-     const speedInput = document.getElementById('playbackSpeed');
-     const speedValue = document.getElementById('speedValue');
-     speedInput.addEventListener('input', (event) => {
-       playbackRate = parseFloat(event.target.value);
-       speedValue.textContent = playbackRate;
-     });
+// 更新播放速度及顯示數字值
+const speedInput = document.getElementById('playbackSpeed');
+const speedValue = document.getElementById('speedValue');
+speedInput.addEventListener('input', (event) => {
+    playbackRate = parseFloat(event.target.value);
+    speedValue.textContent = playbackRate;
+    });
